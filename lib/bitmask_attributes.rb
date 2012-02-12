ede84ec2df9e563083b53131cce85d4b1940e7d8
afly <aflymail@gmail.com>
2012-02-12 22:33:45 +0800

    indexmask

--- lib/bitmask_attributes.rb
+++ lib/bitmask_attributes.rb
@@ -4,17 +4,16 @@ require 'bitmask_attributes/indexmask'
 
 module BitmaskAttributes
   extend ActiveSupport::Concern
-
+  
   module ClassMethods
     def bitmask(attribute, options={multi:false}, &extension)
       unless options[:as] && options[:as].kind_of?(Array)
         raise ArgumentError, "Must provide an Array :as option"
       end
-      if options[:multi] 
+      if options[:multi] then
         bitmask_definitions[attribute] = Definition.new(attribute, options[:as].to_a, &extension)
       else
-
-        bitmask_definitions[attribute] = DefinitionForIndex.new(attribute, option      
+        bitmask_definitions[attribute] = DefinitionForIndex.new(attribute, options[:as].to_a, &extension)
       end
       bitmask_definitions[attribute].install_on(self)      
     end
