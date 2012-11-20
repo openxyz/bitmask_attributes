babc9b347c9f2dda329372795add93c075d468d8
afly <aflymail@gmail.com>
2012-11-20 17:55:45 +0800

    add default value and refact some thing

--- lib/bitmask_attributes.rb
+++ lib/bitmask_attributes.rb
@@ -1,31 +1,49 @@
 require 'bitmask_attributes/definition'
 require 'bitmask_attributes/value_proxy'
-require 'bitmask_attributes/definition_for_index'        
+require 'bitmask_attributes/definition_for_index'
 
 module BitmaskAttributes
   extend ActiveSupport::Concern
-  
+
   module ClassMethods
     def bitmask(attribute, options={multi:false}, &extension)
-      unless options[:as] && options[:as].kind_of?(Array)
-        raise ArgumentError, "Must provide an Array :as option"
-      end
-      if options[:multi] then
-        bitmask_definitions[attribute] = Definition.new(attribute, options[:as].to_a, &extension)
+      # The model cannot be validated if it is preloaded and the attribute/column is not in the
+      # database (the migration has not been run) or table doesn't exist. This usually
+      # occurs in the 'test' and 'production' environment or during migration.
+      if defined?(Rails) && Rails.configuration.cache_classes || !self.table_exists?
       else
-        bitmask_definitions[attribute] = DefinitionForIndex.new(attribute, options[:as].to_a, &extension)
+        unless self.columns.detect { |col| col.name == attribute.to_s }
+          raise ArgumentError, "`#{attribute}' is not an attribute of `#{self}'"
+        end
+
+        unless options[:as] && options[:as].kind_of?(Array)
+          raise ArgumentError, "Must provide an Array :as option"
+        end
+        # be unique, and Array.size < 99 and must not be Integer,eg. [5,3,6]
+        if not options[:default].nil? then
+          unless options[:default_raw] = options[:as].find_index(options[:default])
+            raise ArgumentError, "default value #{options[:default]} is wrong value"
+          end
+        end
       end
-      bitmask_definitions[attribute].install_on(self)      
+
+      if options[:multi] then
+        bitmask_definitions[attribute] = Definition.new(attribute, options[:as], &extension)
+      else
+        bitmask_definitions[attribute] = DefinitionForIndex.new(attribute, options, &extension)
+      end
+      bitmask_definitions[attribute].install_on(self)
     end
-    
+
     def bitmask_definitions
       @bitmask_definitions ||= {}
     end
-    
+
     def bitmasks
       @bitmasks ||= {}
     end
-  end  
+  end
 end
 
 ActiveRecord::Base.send :include, BitmaskAttributes
+
--- lib/bitmask_attributes/definition_for_index.rb
+++ lib/bitmask_attributes/definition_for_index.rb
@@ -1,15 +1,17 @@
 module BitmaskAttributes
-  class DefinitionForIndex 
-    attr_reader :attribute, :values, :extension
-    
-    def initialize(attribute, values=[], &extension)
+  class DefinitionForIndex
+    attr_reader :attribute, :values,:default,:default_raw, :extension
+
+    def initialize(attribute, options, &extension)
       @attribute = attribute
-      @values = values
+      @values = options[:as].to_a
+      @default = options[:default]
+      @default_raw = options[:default_raw]
       @extension = extension
     end
-    
+
     def install_on(model)
-      validate_for model
+      #validate_for model
       #generate_bitmasks_on model
       override model
       #create_convenience_class_method_on model
@@ -17,70 +19,67 @@ module BitmaskAttributes
       create_scopes_on model
       create_attribute_methods_on model
     end
-    
+
     private
 
-      def validate_for(model)
-        # The model cannot be validated if it is preloaded and the attribute/column is not in the
-        # database (the migration has not been run) or table doesn't exist. This usually
-        # occurs in the 'test' and 'production' environment or during migration.
-        return if defined?(Rails) && Rails.configuration.cache_classes || !model.table_exists?
-       
-        unless model.columns.detect { |col| col.name == attribute.to_s }
-          raise ArgumentError, "`#{attribute}' is not an attribute of `#{model}'"
-        end
-      end
-    
-      def generate_bitmasks_on(model)
-        model.bitmasks[attribute] = HashWithIndifferentAccess.new.tap do |mapping|
-          values.each_with_index do |value, index|
-            mapping[value] = index
-          end
-        end
-      end
-    
       def override(model)
         override_getter_on(model)
         override_setter_on(model)
       end
-    
+
+
       def override_getter_on(model)
         model.class_eval %(
-          def #{attribute}            
-            @#{attribute} ||= self.class.bitmask_definitions[:#{attribute}].values[( self[:#{attribute}] || -9999)]
+          def #{attribute}
+            unless @#{attribute} then
+              definition = self.class.bitmask_definitions[:#{attribute}]
+              if self[:#{attribute}]  then
+                @#{attribute} = definition.values[self[:#{attribute}]]
+              else
+                @#{attribute} =  definition.default
+              end
+            end
+            @#{attribute}
           end
+
+          before_create do |m|
+            self[:#{attribute}] ||= self.class.bitmask_definitions[:#{attribute}].default_raw
+          end
+
         )
       end
-    
+
       def override_setter_on(model)
         model.class_eval %(
           def #{attribute}=(raw_value)
-            if raw_value.kind_of?(Integer) then
-              self[:#{attribute}] = raw_value.between?(0,self.class.bitmask_definitions[:#{attribute}].values.size - 1) ? raw_value : -9999
+            definition = self.class.bitmask_definitions[:#{attribute}]
+            if raw_value.kind_of?(Integer) && raw_value.between?(0,definition.values.size - 1) then
+              temp = raw_value
             else
-              self[:#{attribute}] = self.class.bitmask_definitions[:#{attribute}].values.find_index(raw_value) || -9999 
+              temp = definition.values.find_index(raw_value)
             end
-            @#{attribute} = self.class.bitmask_definitions[:#{attribute}].values[self[:#{attribute}] ]
+            self[:#{attribute}] = temp if temp
+            #{attribute}
           end
         )
       end
-    
+
       # Returns the defined values as an Array.
       def create_attribute_methods_on(model)
         #model.class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
         model.class_eval %(
-          def self.values_for_#{attribute}      
-            self.bitmask_definitions[:#{attribute}].values                   
+          def self.values_for_#{attribute}
+            self.bitmask_definitions[:#{attribute}].values
           end
-          
-          def self.values_with_bitmask_for_#{attribute}      
+
+          def self.values_with_bitmask_for_#{attribute}
             ret = []
             self.bitmask_definitions[:#{attribute}].values.each_with_index{|v,k| ret.push [v,k]}
             ret
-          end                                          
+          end
         )
       end
-    
+
       def create_convenience_class_method_on(model)
         model.class_eval %(
           def self.bitmask_for_#{attribute}(*values)
@@ -97,7 +96,7 @@ module BitmaskAttributes
       def create_convenience_instance_methods_on(model)
         values.each do |value|
           model.class_eval %(
-            def #{attribute}_for_#{value}?                  
+            def #{attribute}_for_#{value}?
               self.#{attribute}?(:#{value})
             end
           )
@@ -114,29 +113,30 @@ module BitmaskAttributes
           end
         )
       end
-    
+
       def create_scopes_on(model)
-        model.class_eval %(        
+        model.class_eval %(
           scope :with_#{attribute},
-            proc { |value| 
+            proc { |value|
               if value
                 mask = value.kind_of?(Integer) ? value : self.bitmask_definitions[:#{attribute}].values.find_index(value)
                 where('#{attribute} =  ?', mask )
               else
                 where("#{attribute} >= 0 ")
-              end              
-            }                    
-       
-          scope :without_#{attribute}, 
-            proc { |value| 
+              end
+            }
+
+          scope :without_#{attribute},
+            proc { |value|
               if value
                 mask = value.kind_of?(Integer)  ? value : self.bitmask_definitions[:#{attribute}].values.find_index(value)
                 where('#{attribute} <>  ?', mask )
               else
                 where("#{attribute} IS NULL OR #{attribute} < 0 ")
-              end              
-            }        
+              end
+            }
         )
       end
     end
 end
+
